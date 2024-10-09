eabb588bc4f7cb0a6dcaa5a661fa454345e85b64
abhijayrajvansh <abhijayrajvansh01@gmail.com>
2024-10-09 21:04:09 +0530

    update: search components btn

--- components/Apis.tsx
+++ components/Apis.tsx
@@ -1,21 +1,25 @@
 import React from "react";
-import { Input } from "@/components/ui/input"
+import { Input } from "@/components/ui/input";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import ProductCard from "@/components/structures/ProductCard";
-import { products } from '@/config/product.config'
+import { products } from "@/config/product.config";
+import { Button } from "./ui/button";
 
 const Apis = () => {
   return (
     <div className="px-5">
       <div className="p-4 rounded-lg">
-        <div className="flex items-center gap-5 px-2 mb-6 ">
+        <div className="flex items-center justify-between gap-5 px-2 mb-6 ">
           <div className=" text-2xl font-semibold">Filter By Category</div>
           <div>
-            <Input
-              type="text"
-              placeholder="Search through our APIs"
-              className="bg-gray-100 w-52"
-            />
+            <div className="flex items-center gap-3">
+              <Input
+                type="text"
+                placeholder="Search through our APIs"
+                className="bg-gray-100 w-52"
+              />
+              <Button disabled >Search</Button>
+            </div>
           </div>
         </div>
         <Tabs defaultValue="All Categories">
@@ -26,16 +30,6 @@ const Apis = () => {
             <TabsTrigger value="MOBILE MONEY PAYMENT APIs">
               Mobile Money Payment APIs
             </TabsTrigger>
-            <TabsTrigger value="LIPA NA MPESA (LNM) APIs">
-              Lipa Na Mpesa (LNM) APIs
-            </TabsTrigger>
-            <TabsTrigger value="STATUTORY PAYMENT APIs">
-              Statutory Payment APIs
-            </TabsTrigger>
-            <TabsTrigger value="BILL PAYMENTS APIs">
-              Bill Payments APIs
-            </TabsTrigger>
-            <TabsTrigger value="CARD APIs">CARD APIs</TabsTrigger>
           </TabsList>
 
           <div className="mt-7 bg-gray-100 p-4 rounded-md w-full">
