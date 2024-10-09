59590832bdcf704839c30d8171ba9587ef30bbaf
abhijayrajvansh <abhijayrajvansh01@gmail.com>
2024-10-09 16:51:06 +0530

    update: structuring api json data

--- config/product.config.ts
+++ config/product.config.ts
@@ -9,105 +9,11 @@ import {
 
 export const products = [
   {
-    name: "Corporate Payments",
+    id: "001",
+    category: "Enquiry APIs",
+    name: "Account Enquiry",
     icon: Wallet,
-    rating: 2.8,
-    votes: 14,
-    description: "Transfer money to accounts and external accounts",
-  },
-  {
-    name: "Corporate Collections",
-    icon: PiggyBank,
-    rating: 3.7,
-    votes: 7,
-    description: "Services for recurring deposit and fixed deposit",
-  },
-  {
-    name: "UPI",
-    icon: Smartphone,
-    rating: 3.5,
-    votes: 8,
-    description: "Services for UPI transactions",
-  },
-  {
-    name: "Bill Payments",
-    icon: Banknote,
-    rating: 5,
-    votes: 3,
-    description: "Services for Bill Payments",
-  },
-  {
-    name: "Corporate Services",
-    icon: Landmark,
-    rating: 4,
-    votes: 2,
-    description: "Services for recurring deposit and fixed deposit",
-  },
-  {
-    name: "Loans",
-    icon: CreditCard,
-    rating: 2,
-    votes: 2,
-    description: "Services for Loan management",
-  },
-  {
-    name: "Credit Card",
-    icon: CreditCard,
-    rating: 3.7,
-    votes: 3,
-    description: "Services for credit card management",
-  },
-  {
-    name: "Deposits",
-    icon: PiggyBank,
-    rating: 3.5,
-    votes: 2,
-    description: "Services for recurring deposit and fixed deposit",
-  },
-
-
-
-
-  {
-    name: "Corporate Payments",
-    icon: Wallet,
-    rating: 2.8,
-    votes: 14,
-    description: "Transfer money to accounts and external accounts",
-  },
-  {
-    name: "Corporate Collections",
-    icon: PiggyBank,
-    rating: 3.7,
-    votes: 7,
-    description: "Services for recurring deposit and fixed deposit",
-  },
-  {
-    name: "UPI",
-    icon: Smartphone,
-    rating: 3.5,
-    votes: 8,
-    description: "Services for UPI transactions",
-  },
-  {
-    name: "Bill Payments",
-    icon: Banknote,
-    rating: 5,
-    votes: 3,
-    description: "Services for Bill Payments",
-  },
-  {
-    name: "Corporate Services",
-    icon: Landmark,
-    rating: 4,
-    votes: 2,
-    description: "Services for recurring deposit and fixed deposit",
-  },
-  {
-    name: "Loans",
-    icon: CreditCard,
-    rating: 2,
-    votes: 2,
-    description: "Services for Loan management",
+    description: "Fetch account balance in real-time.",
+    docs: ""
   }
 ];
