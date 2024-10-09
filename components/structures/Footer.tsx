86e4a3a6f924e0c8041dde7dbcb87d77d316b9df
abhijayrajvansh <abhijayrajvansh01@gmail.com>
2024-10-09 21:00:07 +0530

    update: footer components

--- components/structures/Footer.tsx
+++ components/structures/Footer.tsx
@@ -42,7 +42,7 @@ const Footer = () => {
           </div>
         </div>
       </section>
-      <footer className="flex flex-col gap-2 sm:flex-row py-6 w-full shrink-0 items-center px-4 md:px-6 border-t">
+      <footer className="flex flex-col gap-2 sm:flex-row py-6 w-full shrink-0 items-center px-4 md:px-6">
         <p className="text-gray-500">
           © 2024 OpenBankAPI. All rights reserved.
         </p>
