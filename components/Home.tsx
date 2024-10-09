b1c0f23c9c961b4aab30a31546d3fa313c5cb7cf
abhijayrajvansh <abhijayrajvansh01@gmail.com>
2024-10-09 21:06:02 +0530

    update: minor changes

--- components/Home.tsx
+++ components/Home.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Button } from './ui/button'
 import Link from 'next/link'
-import { Code, Database, Key, Lock, Server, Zap } from "lucide-react"
+import { Lock, Server, Zap } from "lucide-react"
 
 const Home = () => {
   return (
