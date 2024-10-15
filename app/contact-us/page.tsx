637f8f3ed38673ff9ad4c57dbe0d09c9dd2440a4
abhijayrajvansh <abhijayrajvansh01@gmail.com>
2024-10-15 11:28:45 +0530

    update: added new contact-us form page

--- app/contact-us/page.tsx
+++ app/contact-us/page.tsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import React from "react";
+import ContactUs from "@/components/pages/ContactUs";
+import Image from "next/image";
 
 const page = () => {
   return (
-    <div>
-      contact us
-    </div>
-  )
-}
+    <div className="min-h-screen flex flex-col justify-center items-center container mx-auto">
+      <div className="flex items-center justify-around w-full">
+        <ContactUs />
 
-export default page
+        <div>
+          <Image height={600} width={600}
+            src="/images/flat-design-illustration-customer-support_23-2148887720.avif"
+            alt="Contact Illustration"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default page;
